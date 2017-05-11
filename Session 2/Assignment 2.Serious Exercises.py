1e1321bc770a746853e01003523e76a33524885c
NOTHING-CCSCFRQ\Administrator <vutrongquang.ftu@gmail.com>
2017-05-11 19:58:35 +0700

    Add = and change code <=

--- Session 2/Assignment 2.Serious Exercises.py
+++ Session 2/Assignment 2.Serious Exercises.py
@@ -8,11 +8,11 @@ bmi = mass_kg/(hei_m * hei_m)
 
 if bmi <16:
     print ("Severely underweight")
-elif bmi > 16 and bmi < 18.5:
+elif  bmi <= 18.5:
     print ("Underweight")
-elif bmi > 18.5 and bmi < 25:
+elif  bmi <= 25:
     print ("Normal")
-elif bmi > 25 and bmi <30:
+elif  bmi <= 30:
     print ("Overweight")
 else:
     print ("Obese")
